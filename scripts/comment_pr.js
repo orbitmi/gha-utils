2b3f51bcd499ab9e5fb30e95d66a5f01c0f33976
Jovica Zlatanovic <jovica.zlatanovic@htecgroup.com>
2022-03-01 12:00:32 +0100

    update: Comment PR to support read from file

--- scripts/comment_pr.js
+++ scripts/comment_pr.js
@@ -1,9 +1,17 @@
+const fs = require('fs')
+
 module.exports = ({github, context, pr_msg}) => {
-    if (context.eventName == "pull_request") {
-        const { issue: { number: issue_number }, repo: { owner, repo }  } = context;
-        const body = decodeURI(`👋 Hey!\n ${pr_msg}`);
-        github.rest.issues.createComment({ issue_number, owner, repo, body });
+  if (context.eventName == "pull_request") {
+    let body
+    const {issue: {number: issue_number}, repo: {owner, repo}} = context;
+    if (pr_msg.startsWith('file::')) {
+      body = fs.readFileSync(pr_msg.split('::')[1], 'utf8')
     } else {
-        console.log("Not a PR")
+      body = decodeURI(`👋 Hey!\n ${pr_msg}`);
     }
+
+    github.rest.issues.createComment({issue_number, owner, repo, body});
+  } else {
+    console.log("Not a PR")
+  }
 }
