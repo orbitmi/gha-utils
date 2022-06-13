3634d55b995a9da3741dcd2ee309d602175e9413
Nikola Grabovica <nikola.grabovica@htecgroup.com>
2022-06-13 12:17:27 +0200

    update: repo_data.sh app_version formatting for experimental branches

--- scripts/repo_data.sh
+++ scripts/repo_data.sh
@@ -38,7 +38,7 @@ resolve_app_version() {
       fi
       if [[ -z "${GITHUB_REF##*/poc/*}" ]]; then
         set_var APP_RELEASE_TYPE "Experimental"
-        set_var APP_VERSION "${GITHUB_REF##refs/heads/poc/}-rc.${GITHUB_RUN_NUMBER}"
+        set_var APP_VERSION "experimental-${GITHUB_SHA::7}"
       fi
       if [[ -z "${GITHUB_REF##*/tags/*}" ]]; then
         set_var APP_RELEASE_TYPE "Production"
