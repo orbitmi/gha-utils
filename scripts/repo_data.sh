83813a28c43067bfb4df6acbbdec6183862a05ea
Alexey Dyachkov <ad@tbffinance.com>
2023-10-03 15:50:31 +0200

    OR-526 Update repo_data.sh

--- scripts/repo_data.sh
+++ scripts/repo_data.sh
@@ -19,7 +19,8 @@ resolve_app_version() {
     # If HEAD_REF is not NULL and Base branch doesn't contain 'pull' - then it's production
     if [[ -n "${GITHUB_HEAD_REF}" ]]; then
     	echo " >>> Pull Request Detected."
-        if [[ -z "${GITHUB_REF##main}" ]] || [[ -z "${GITHUB_REF##master}" ]]; then
+	echo "GITHUB_REF: ${GITHUB_REF}"
+        if [[ -z "${GITHUB_REF##*/main}" ]] || [[ -z "${GITHUB_REF##*/master}" ]]; then
             set_var APP_RELEASE_TYPE "Production"
             set_var APP_VERSION "${GITHUB_HEAD_REF##release/}"
         fi
